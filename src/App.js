1c74306a478267678b8e81b75623b5796a2d56cc
Ryan <omgitsgod@gmail.com>
2020-10-14 11:45:41 -0400

    Add backdrop, topnav and speedDial menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Backdrop } from '@material-ui/core'
+import TopNav from './TopNav';
+import SpeedDialMenu from './SpeedDialMenu';
 import logo from './paws.png';
 import './App.css';
 
 function App() {
+  const [backdropStatus, setBackdropStatus] = useState(false)
+  const handleBackdrop = (bool) => {
+    setBackdropStatus(bool);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" data-testid="paws" alt="paws" />
-        <p>
-          paws for pals
-        </p>
+    <div className='App'>
+      <Backdrop open={backdropStatus} />
+      <TopNav />
+      <header className='App-header'>
+        <img src={logo} className='App-logo' data-testid='paws' alt='paws' />
+        <p>paws for pals</p>
       </header>
+      <SpeedDialMenu handleBackdrop={handleBackdrop} />
     </div>
   );
 }
