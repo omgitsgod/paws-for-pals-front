8f5296368b49cfdedb6ce2afede91bd4294b0348
Ryan <omgitsgod@gmail.com>
2020-11-30 11:40:51 -0500

    Fix formatting

--- src/useGetPets.js
+++ src/useGetPets.js
@@ -14,33 +14,33 @@ const dataFetchReducer = (state, action) => {
       return {
         ...state,
         data: [],
-        type: action.payload
+        type: action.payload,
       };
     case 'SET_OPTIONS':
       return {
         ...state,
         data: [],
-        options: action.payload
-      }
+        options: action.payload,
+      };
     case 'FETCH_INIT':
-      return { 
+      return {
         ...state,
         isLoading: true,
         isError: false,
-        data: []
+        data: [],
       };
     case 'FETCH_SUCCESS':
-      return { 
+      return {
         ...state,
         isLoading: false,
         isError: false,
-        data: action.payload
+        data: action.payload,
       };
     case 'FETCH_FAILURE':
-      return { 
+      return {
         ...state,
         isLoading: false,
-        isError: true
+        isError: true,
       };
     default:
       throw new Error();
@@ -57,12 +57,12 @@ function useGetPets(initialType, initialOptions) {
   });
 
   const setOptions = (options) => {
-    dispatch({type: 'SET_OPTIONS', payload: options});
-  }
+    dispatch({ type: 'SET_OPTIONS', payload: options });
+  };
 
   const setType = (type) => {
-    dispatch({type: 'SET_TYPE', payload: type});
-  }
+    dispatch({ type: 'SET_TYPE', payload: type });
+  };
 
   useEffect(() => {
     const getPets = async () => {
@@ -73,13 +73,13 @@ function useGetPets(initialType, initialOptions) {
         const result = await fetch(url).then((r) => r.json());
         const filtered = result.animals.filter((pet) => pet.photos[0]);
         const photos = filtered.map((pet) => pet.photos[0].full);
-      //  setPets(filtered);
+        //  setPets(filtered);
         dispatch({ type: 'FETCH_SUCCESS', payload: photos });
         console.log(photos);
       } catch (error) {
         dispatch({ type: 'FETCH_FAILURE' });
       }
-    }
+    };
 
     getPets();
   }, [state.type, handleOptions(state.options)]);
