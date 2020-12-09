730eba419d075553c74c5927109a9f4bb49eb5e3
Ryan <omgitsgod@gmail.com>
2020-12-09 11:10:35 -0500

    Add Architects Daughter as global font

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import dotenv from 'dotenv';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 dotenv.config();
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Architects Daughter", cursive',
+    button: {
+      textTransform: 'none',
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
