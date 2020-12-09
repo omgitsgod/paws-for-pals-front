9117c20af67df6dcc43969eaf28a87c8af6a0b7e
Ryan <omgitsgod@gmail.com>
2020-12-09 14:19:30 -0500

    Add geolocation option

--- src/ZipCodeModal.js
+++ src/ZipCodeModal.js
@@ -1,97 +1,79 @@
 import React from 'react';
 import { Typography, Modal, Backdrop, Fade, FormControl, InputLabel, Input, IconButton, Button, Divider } from '@material-ui/core';
-import  { GpsFixedTwoTone as LocationIcon } from '@material-ui/icons';
+import { GpsFixedTwoTone as LocationIcon } from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
 
-
 function ZipCodeModal(props) {
+  const { classes, open, setOpen, setZip } = props;
 
-    const { classes, open, setOpen, setZip } = props;
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleClose = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        setOpen(false);
-    }
+    const zipCode = e.target.zipcode.value;
+    zipCode.match(/^\d{5}(-\d{4})?$/) ? setZip(zipCode) : console.log('nope');
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleLocation = async (position) => {
+    const { latitude, longitude } = position.coords;
+    const location = `${latitude},${longitude}`;
+    setZip(location);
+  };
 
-        const zipCode = e.target.zipcode.value;
-        zipCode.match(/^\d{5}(-\d{4})?$/) ? setZip(zipCode) : console.log('nope')
-    };
-
-    const handleLocation = async (position) => {
-
-        const { latitude, longitude } = position.coords;
-        const json = await fetch(`${process.env.REACT_APP_BACK_HOST}/zip/${latitude}/${longitude}`).then(r => r.json());
-        const location = json.Response.View[0].Result[0].Location.Address;
-
-        console.log(location.PostalCode);
-        setZip(location.PostalCode);
-    }
-
-    return (
-        <Modal
-            aria-labelledby='transition-modal-title'
-            aria-describedby='transition-modal-description'
-            className={classes.modal}
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-                timeout: 500,
-            }}
-        >
-            <Fade in={open}>
-                <div className={classes.paper}>
-                    <Typography variant='h5'>
-                        Enter Zip Code
-                    </Typography>
-                    <form className={classes.form} onSubmit={handleSubmit}>
-                        <FormControl margin='normal' required fullWidth>
-                        <InputLabel htmlFor='zipcode'>Zip Code</InputLabel>
-                        <Input
-                            id='zipcode'
-                            name='zipcode'
-                            autoFocus
-                            autoComplete='off'
-                        />
-                        <Button
-                            type='submit'
-                            fullWidth
-                            variant='contained'
-                        //   color='primary' 
-                            className={classes.submit}
-                            style={{ background: 'transparent', boxShadow: 'none' }}
-                        >
-                            Submit
-                        </Button>
-                        </FormControl>
-                    </form>
-        {/*    <Divider /> */}
-                {/*    <Typography variant='h5'>
-                        Allow Access To Location
-                    </Typography>
-                    <IconButton
-                        aria-haspopup='true'
-                        color='primary'
-                        onClick={() =>
-                        navigator.geolocation.getCurrentPosition(handleLocation)
-                        }
-                    >
-                        <LocationIcon />
-                        Find Me
-                    </IconButton>
-                    */}
-                </div>
-            </Fade>
-        </Modal>
-    );
+  return (
+    <Modal
+      aria-labelledby='transition-modal-title'
+      aria-describedby='transition-modal-description'
+      className={classes.modal}
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <div className={classes.paper}>
+          <Typography variant='h5'>Enter Zip Code</Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <FormControl margin='normal' required fullWidth>
+              <InputLabel htmlFor='zipcode'>Zip Code</InputLabel>
+              <Input id='zipcode' name='zipcode' autoComplete='off' />
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                //   color='primary'
+                className={classes.submit}
+                style={{ background: 'transparent', boxShadow: 'none' }}
+              >
+                Submit
+              </Button>
+            </FormControl>
+          </form>
+          <Divider />
+          <Typography variant='h5'>Allow Access To Location</Typography>
+          <IconButton
+            aria-haspopup='true'
+            color='primary'
+            onClick={() =>
+              navigator.geolocation.getCurrentPosition(handleLocation)
+            }
+          >
+            <LocationIcon />
+            <Typography variant='h5'>Find Me</Typography>
+          </IconButton>
+        </div>
+      </Fade>
+    </Modal>
+  );
 }
 
 const styles = (theme) => ({
-
   modal: {
     display: 'flex',
     alignItems: 'center',
