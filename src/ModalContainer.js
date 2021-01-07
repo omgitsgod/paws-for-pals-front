1cfe83e2480f7b89e33ebe4270781c20ff87c8b5
Ryan <omgitsgod@gmail.com>
2021-01-07 11:27:58 -0500

    Add container for modals

---
+++ src/ModalContainer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Modal, Backdrop } from '@material-ui/core';
+import OptionsModal from './OptionsModal';
+import { withStyles } from '@material-ui/core/styles';
+
+function ModalContainer(props) {
+  const { classes, open, setOpen, setAnimal, setOptions } = props;
+
+  return (
+    <Modal
+      aria-labelledby='transition-modal-title'
+      aria-describedby='transition-modal-description'
+      className={classes.modal}
+      open={open}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <>
+        <OptionsModal
+          open={open}
+          setOpen={setOpen}
+          setOptions={setOptions}
+          setAnimal={setAnimal}
+        />
+      </>
+    </Modal>
+  );
+}
+
+const styles = (theme) => ({
+  modal: {
+    height: '-200px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+});
+
+export default withStyles(styles)(ModalContainer);
