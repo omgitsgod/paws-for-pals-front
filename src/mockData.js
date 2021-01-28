d1d4e7575216f2494a5468a6b0fe67ba6ce96ebc
Ryan <omgitsgod@gmail.com>
2021-01-27 22:06:28 -0500

    Add mockData for testing

---
+++ src/mockData.js
@@ -0,0 +1,92 @@
+const cards = [
+  {
+    age: 'Young',
+    attributes: {
+      spayed_neutered: true,
+      house_trained: true,
+      declawed: null,
+      special_needs: false,
+      shots_current: true,
+    },
+    breeds: {
+      primary: 'Labrador Retriever',
+      secondary: null,
+      mixed: false,
+      unknown: false,
+    },
+    coat: 'Short',
+    colors: { primary: 'Black', secondary: null, tertiary: null },
+    contact: {
+      email: 'harescue1@aol.com',
+      phone: '(347) 722-5939',
+      address: {
+        address1: 'PO Box 170289',
+        address2: null,
+        city: 'Ozone Park',
+        country: 'US',
+        postcode: '11417',
+        state: 'NY',
+      },
+    },
+    description:
+      'Romeo is a 2 year old lab mix. He is housebroken, good with dogs, and loves to go on walks....',
+    environment: { children: null, dogs: true, cats: null },
+    gender: 'Male',
+    id: 50374019,
+    name: 'Romeo Go',
+    organization_animal_id: null,
+    organization_id: 'NY1047',
+    photos: [
+      ({
+        full:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315',
+        large:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315&width=600',
+        medium:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315&width=300',
+        small:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315&width=100',
+      },
+      {
+        full:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/2/?bust=1611716320',
+        large:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/2/?bust=1611716320&width=600',
+        medium:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/2/?bust=1611716320&width=300',
+        small:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/2/?bust=1611716320&width=100',
+      },
+      {
+        full:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/3/?bust=1611716327',
+        large:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/3/?bust=1611716327&width=600',
+        medium:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/3/?bust=1611716327&width=300',
+        small:
+          'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/3/?bust=1611716327&width=100',
+      }),
+    ],
+    primary_photo_cropped: {
+      small:
+        'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315&width=300',
+      medium:
+        'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315&width=450',
+      large:
+        'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315&width=600',
+      full:
+        'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50374019/1/?bust=1611716315',
+    },
+    size: 'Medium',
+    species: 'Dog',
+    status: 'adoptable',
+    tags: [],
+    type: 'Dog',
+    url:
+      'https://www.petfinder.com/dog/romeo-50374019/ny/ozone-park/heavenly-angels-animal-rescue-ny1047/?referrer_id=60af93ce-4c7a-4bf6-aa32-1983824d483f',
+    videos: [],
+  },
+];
+
+export default cards;
