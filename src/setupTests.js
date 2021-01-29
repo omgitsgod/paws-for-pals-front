c4d781417032c9910f640389275cc17ae6e5af96
Ryan <omgitsgod@gmail.com>
2021-01-29 10:59:25 -0500

    Add beforeEach and AfterEach

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,13 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { toHaveNoViolations } from 'jest-axe';
+import cards from './__tests__/mockData';
+
+expect.extend(toHaveNoViolations);
+beforeEach(() => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue({ animals: cards }),
+  });
+});
+afterEach(() => {
+  jest.restoreAllMocks();
+});
