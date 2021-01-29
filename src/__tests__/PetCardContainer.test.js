369afd53a3af0e6ed3eb5506401855a0e4266a18
Ryan <omgitsgod@gmail.com>
2021-01-29 10:52:02 -0500

    Move PetCardContainer.test to /__tests__

--- src/__tests__/PetCardContainer.test.js
+++ src/__tests__/PetCardContainer.test.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { axe, toHaveNoViolations } from 'jest-axe';
-import PetCardContainer from './PetCardContainer';
-import cards from './mockData';
+import { axe } from 'jest-axe';
+import PetCardContainer from '../Components/PetCardContainer';
 
-expect.extend(toHaveNoViolations);
-beforeEach(() => {
-  jest.spyOn(global, 'fetch').mockResolvedValue({
-    json: jest.fn().mockResolvedValue({animals: cards}),
-  });
-});
-afterEach(() => {
-  jest.restoreAllMocks();
-});
-
-describe('PetCardContainer', async () => {
+describe('PetCardContainer', () => {
   test('should not contain accessibility violations', async () => {
     const { container } = render(<PetCardContainer />);
     await screen.findByText('Romeo Go');
