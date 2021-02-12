9b5b4a5d0ac80fd54317c950e023002c9f889f7c
Ryan <omgitsgod@gmail.com>
2021-02-12 11:26:20 -0500

    Add user and authenticated state & add login and logout

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,7 @@ import SpeedDialMenu from './SpeedDialMenu';
 import PetCardContainer from './PetCardContainer';
 import ModalContainer from './ModalContainer';
 import '../App.css';
+import { backHost, removeHash } from '../config';
 
 function App() {
   const [options, setOptions] = useState({
@@ -15,23 +16,48 @@ function App() {
       senior: true,
     },
   });
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [modal, setModal] = useState(true);
+  
   console.log(options)
-  const wakeUp = () => {
-    fetch(process.env.REACT_APP_BACK_HOST);
+  const login = (user) => {
+    setUser(user);
+    setIsAuthenticated(true);
+    console.log('currently logged in as: ', user.name);
   };
+
+  const handleLogin = async () => {
+    const user = await fetch(backHost, {
+      method: 'GET',
+      credentials: 'include',
+    }).then((r) => r.json());
+    if (user.email) {
+      login(user);
+    }
+  };
+
+  const handleLogout = () => {
+    fetch(`${backHost}/logout`, {
+      method: 'GET',
+      credentials: 'include',
+    });
+    setIsAuthenticated(false);
+    setUser(null);
+  };  
   const onClickActions = {
     changeOptions: () => setModal(true),
     getDogs: () => setOptions({...options, type: 'Dog'}),
     getCats: () => setOptions({...options, type: 'Cat'}),
   };
   useEffect(() => {
-    wakeUp();
+    removeHash();
+    handleLogin();
   }, []);
 
   return (
     <div className='App'>
-      {!modal ? <TopBar /> : null}
+      {!modal ? <TopBar isAuthenticated={isAuthenticated} user={user} handleLogout={handleLogout} /> : null}
       <header className='Content'>
         {!modal ? (
           <PetCardContainer key={options.type} type={options.type} options={options} />
