5ec07c80add9d87c7c2ad5189965300bf958f1b2
Ryan <omgitsgod@gmail.com>
2021-03-26 11:21:35 -0400

    Add selected, pet, add localStorage to options and modal and add BottomNav

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import TopBar from './TopBar';
 import SpeedDialMenu from './SpeedDialMenu';
+import BottomNav from './BottomNav'
 import PetCardContainer from './PetCardContainer';
 import ModalContainer from './ModalContainer';
 import '../App.css';
 import { backHost, removeHash } from '../config';
 
 function App() {
-  const [options, setOptions] = useState({
+  const [options, setOptions] = useState(localStorage.getItem('options') ? JSON.parse(localStorage.getItem('options')) : {
     type: 'Dog',
     age: {
       baby: true,
@@ -18,15 +19,16 @@ function App() {
   });
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [modal, setModal] = useState(true);
-  
+  const [modal, setModal] = useState(localStorage.getItem('options') ? false : true);
+  const [selected, setSelected] = useState('list');
+  const [pet, setPet] = useState({});
+
   console.log(options)
   const login = (user) => {
     setUser(user);
     setIsAuthenticated(true);
     console.log('currently logged in as: ', user.name);
   };
-
   const handleLogin = async () => {
     const user = await fetch(backHost, {
       method: 'GET',
@@ -36,7 +38,6 @@ function App() {
       login(user);
     }
   };
-
   const handleLogout = () => {
     fetch(`${backHost}/logout`, {
       method: 'GET',
@@ -44,7 +45,12 @@ function App() {
     });
     setIsAuthenticated(false);
     setUser(null);
-  };  
+  };
+  const handlePet = (data, liked, disliked) => {
+    const less = data.length - 1 - liked - disliked;
+    console.log('less: ', less)
+    setPet(less >= 0 ? data[less] : {})
+  }
   const onClickActions = {
     changeOptions: () => setModal(true),
     getDogs: () => setOptions({...options, type: 'Dog'}),
@@ -54,17 +60,36 @@ function App() {
     removeHash();
     handleLogin();
   }, []);
-
+  console.log(selected)
   return (
     <div className='App'>
-      {!modal ? <TopBar isAuthenticated={isAuthenticated} user={user} handleLogout={handleLogout} /> : null}
+      {!modal ? (
+        <TopBar
+          isAuthenticated={isAuthenticated}
+          user={user}
+          handleLogout={handleLogout}
+        />
+      ) : null}
       <header className='Content'>
         {!modal ? (
-          <PetCardContainer key={options.type} type={options.type} options={options} />
+          <PetCardContainer
+            key={options.type}
+            type={options.type}
+            options={options}
+            handlePet={handlePet}
+            pet={pet}
+            selected={selected}
+          />
         ) : (
-          <ModalContainer open={modal} setOpen={setModal} setOptions={setOptions} initialOptions={options} />
+          <ModalContainer
+            open={modal}
+            setOpen={setModal}
+            setOptions={setOptions}
+            initialOptions={options}
+          />
         )}
         <SpeedDialMenu onClickActions={onClickActions} />
+        <BottomNav selected={selected} setSelected={setSelected} pet={pet} />
       </header>
     </div>
   );
