0f68b0ff4f32d4cd3d30a67df69f148926d4ec2b
Ryan <omgitsgod@gmail.com>
2021-04-08 11:02:56 -0400

    Change position of FAB

--- src/Components/SpeedDialMenu.js
+++ src/Components/SpeedDialMenu.js
@@ -13,8 +13,8 @@ const useStyles = makeStyles((theme) => ({
   },
   speedDial: {
     position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
+    bottom: theme.spacing(8),
+    right: theme.spacing(1),
   },
 }));
 
