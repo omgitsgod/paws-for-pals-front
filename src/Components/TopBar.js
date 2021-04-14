8d479f10c76a22cf6541daef35ec25a305ebda7b
Ryan <omgitsgod@gmail.com>
2021-04-14 16:52:59 -0400

    Shrink avatar size

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -15,8 +15,8 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   avatar: {
-    height: theme.spacing(6),
-    width: theme.spacing(6),
+    height: theme.spacing(5),
+    width: theme.spacing(5),
     float: 'right',
     borderRadius: '50%',
   },
