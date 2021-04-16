8d5bfa866c8317d267173d1edbc2fd0c1b31f048
Ryan <omgitsgod@gmail.com>
2021-04-16 11:10:31 -0400

    Remove theme wrap from index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import dotenv from 'dotenv';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './index.css';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 
 dotenv.config();
 
-const primaryFont = '"Architects Daughter", cursive';
-const secondaryFont = '"Shadows Into Light", cursive';
-
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: primaryFont,
-    button: {
-      textTransform: 'none',
-    },
-  },
-});
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
