e5ed4850ad4081977559fdf349077f03625303e1
Ryan <omgitsgod@gmail.com>
2021-04-17 14:02:16 -0400

    Add darkmode and handleDarkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { useMediaQuery } from '@material-ui/core';
+
+function useDarkMode() {
+  const preferDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem('darkMode') === 'true' ? true : preferDarkMode ? true : false
+  );
+
+  const handleDarkMode = () => {
+    setDarkMode(darkMode => {
+      const newDarkMode = !darkMode;
+      localStorage.setItem('darkMode', newDarkMode);
+      return newDarkMode;
+    });
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem('darkMode')) {
+    setDarkMode(preferDarkMode);
+    }
+  }, [preferDarkMode]);
+  
+  return [darkMode, handleDarkMode];
+}
+
+export default useDarkMode;
