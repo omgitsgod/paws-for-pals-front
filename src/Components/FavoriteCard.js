576b5a9567847637bdfe4476169d8f3b789c2896
Ryan <omgitsgod@gmail.com>
2021-04-29 11:26:09 -0400

    Add card for favorites

---
+++ src/Components/FavoriteCard.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useSpring, a } from '@react-spring/web';
+import { backHost } from '../config';
+
+const useStyles = makeStyles({
+  c: {
+    position: 'absolute',
+    cursor: 'pointer',
+    willChange: 'transform, opacity',
+    margin: '20px',
+  },
+  back: {
+    backgroundSize: 'cover',
+    backgroundImage: (fav) => `url(${fav.primary_photo_cropped.small})`,
+    height: '380px',
+    width: '85%',
+  },
+  front: {
+    backgroundSize: 'cover',
+    backgroundImage: (fav) =>
+      `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${fav.primary_photo_cropped.small})`,
+    height: '380px',
+    width: '85%',
+  },
+});
+
+function FavoriteCard({ fav, fetchFavorites }) {
+  const classes = useStyles(fav);
+  const [flipped, setFlipped] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+  const deleteFavorite = async (item) => {
+    const url = `${backHost}/delete_favorite?id=${item}`;
+    const data = await fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+    }).then(fetchFavorites);
+  };
+  return (
+    <Grid item xs={12} sm={3}>
+      <div style={{position: 'relative'}}>
+      <a.div
+        className={`${classes.c} ${classes.back}`}
+        style={{ opacity: opacity.to((o) => 1 - o), transform }}
+        onClick={() => setFlipped((state) => !state)}
+      >
+      </a.div>
+      <a.div
+        className={`${classes.c} ${classes.front}`}
+        style={{
+          opacity,
+          transform,
+          rotateX: '180deg',
+        }}
+        onClick={() => setFlipped((state) => !state)}
+      >
+        <button onClick={() => deleteFavorite(fav.id)}>Delete</button>
+      </a.div>
+      </div>
+    </Grid>
+  );
+}
+
+export default FavoriteCard;
