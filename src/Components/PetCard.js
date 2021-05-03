6102823ece181ccb050a12ae70cff5159f752888
Ryan <omgitsgod@gmail.com>
2021-05-03 11:24:50 -0400

    Return Card with source deck

--- src/Components/PetCard.js
+++ src/Components/PetCard.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Typography, Icon } from '@material-ui/core';
-import LocationOn from '@material-ui/icons/LocationOn';
 import { makeStyles } from '@material-ui/core/styles';
-import { animated, to as interpolate } from 'react-spring';
+import { animated } from 'react-spring';
+import Card from './Card';
 
 const useStyles = makeStyles((theme) => ({
   organize: {
@@ -14,71 +13,15 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '20vh',
     justifyContent: 'center',
   },
-  img: {
-    height: '380px',
-    width: '85%',
-    userDrag: 'none',
-  },
-  distance: {
-    color: 'gray',
-  },
-  description: {
-    color:
-      theme.palette.type === 'light' ? '#393535' : 'rgba(255, 255, 255, 0.7)',
-  },
-  card: {
-    backgroundColor: theme.palette.type === 'light' ? 'white' : '#424242',
-    padding: '5px',
-    width: '45vh',
-    overflow: 'hidden',
-    maxWidth: '300px',
-    height: '85vh',
-    maxHeight: '570px',
-    willChange: 'transform',
-    color: 'black',
-    borderRadius: '10px',
-    boxShadow:
-      theme.palette.type === 'light' ? '0 12.5px 100px -10px rgba(50, 50, 73, 0.4), 0 10px 10px -10px rgba(50, 50, 73, 0.3)' : null,
-    borderColor: 'rgba(255, 255, 255, 0.7)',
-    borderStyle: 'solid'
-  },
 }));
 
 const PetCard = ({ i, x, y, rot, scale, trans, bind, cards }) => {
   const classes = useStyles();
   const card = cards[i];
-  const photo = card.photos[0].full;
-  const { name, distance, description } = card;
-  const nameSpaceCount = name.split(' ').length - 1;
-  const nameVariant = nameSpaceCount > 1 ? 'h5' : 'h4';
+  const spring = {rot, scale, trans, bind, i, x, y}
 
   return (
-    <animated.div className={classes.organize} key={i} style={{ x, y }}>
-      <animated.div
-        className={classes.card}
-        {...bind(i)}
-        style={{
-          transform: interpolate([rot, scale], trans),
-        }}
-      >
-        <img src={photo} className={classes.img} alt='Pal' />
-        <Typography variant={nameVariant} align='left'>
-          {name}
-        </Typography>
-        {distance ? (
-          <Typography className={classes.distance} variant='h6' align='left'>
-            <Icon>
-              <LocationOn />
-            </Icon>
-            {distance > 1 ? distance.toFixed(2) : '< 1'} miles away
-          </Typography>
-        ) : null}
-
-        <Typography className={classes.description} align='left'>
-          {description}
-        </Typography>
-      </animated.div>
-    </animated.div>
+    <Card card={card} source='deck' spring={spring} />
   );
 };
 
